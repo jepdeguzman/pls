76ac8b02591930cd3d68ea2c98bd3e42e093b696
jepdeguzman <jeremy.deguzman@pointwest.com.ph>
2018-02-05 07:42:51 +0800

    First Commit

---
+++ src/com/corejava/pointwest/pls/beans/Data.java
@@ -0,0 +1,15 @@
+package com.corejava.pointwest.pls.beans;
+
+public class Data {
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/com/corejava/pointwest/pls/main/Main.java
@@ -0,0 +1,14 @@
+package com.corejava.pointwest.pls.main;
+
+import com.corejava.pointwest.pls.beans.Data;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	//System.out.println("ASDFASDASD");
+		
+		
+		
+	}
+
+}
